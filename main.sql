0fbd3471e11d09a836022ccddceeca4dd436d6f9
Noe Amador <52434895+noeamadorg@users.noreply.github.com>
2022-04-05 03:01:11 +0000

    test commit and comments

--- main.sql
+++ main.sql
@@ -1,13 +1,15 @@
-
 SELECT "Hello world!";
-
-
 
 SELECT *
 FROM parque_diversiones;
+
 
 SELECT idPedido, ROUND(AVG(precioLinea),4)
 FROM LINEAPEDIDO
 GROUP BY idPedido
 HAVING AVG(precioLinea) < 10
 
+--CODE SELECT * 
+--FROM Customers 
+--WHERE Region IN(‘CA’, ‘MT’, ‘WA’)
+--ORDER BY Region, CompanyName DESC
