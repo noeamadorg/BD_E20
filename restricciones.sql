27a12952f76661658a95a46ea54f4e8885a2ee7c
Noe Amador <52434895+noeamadorg@users.noreply.github.com>
2022-04-08 03:44:46 +0000

    22:44

--- restricciones.sql
+++ restricciones.sql
@@ -23,6 +23,15 @@ ALTER TABLE cfabricante
 ALTER TABLE cliente 
   ADD CONSTRAINT pk_cliente_id_cliente PRIMARY KEY (id_cliente);
 
+ALTER TABLE cmunicipio
+  ADD CONSTRAINT pk_cmunicipio_id_municipio PRIMARY KEY (id_municipio);
+
+ALTER TABLE crol
+  ADD CONSTRAINT pk_crol_id_rol PRIMARY KEY (id_crol);
+
+ALTER TABLE csexo
+  ADD CONSTRAINT pk_csexo_id_sexo PRIMARY KEY (id_sexo);
+
 ALTER TABLE ctipo_articulo 
   ADD CONSTRAINT pk_ctipo_articulo_id_tipo_articulo PRIMARY KEY (id_tipo_articulo);
 
@@ -51,21 +60,19 @@ ALTER TABLE orden_cliente
   ADD CONSTRAINT pk_orden_cliente_id_orden_cliente PRIMARY KEY (id_orden_cliente);
 
 ALTER TABLE persona
-  ADD CONSTRAINT pk_id_persona PRIMARY KEY (id_persona);
+  ADD CONSTRAINT pk_persona_id_persona PRIMARY KEY (id_persona);
 
-
-
-
+---COMPLETAS---
 
 ------------------  LLAVES SECUNDARIAS ----------------------------------
 ALTER TABLE atraccion
   ADD CONSTRAINT fk_atraccion_id_frabricante_cfabricante_id_fabricante FOREIGN KEY (id_fabricante) REFERENCES cfabricante(id_fabricante);
 
 ALTER TABLE atraccion
-  ADD CONSTRAINT fk_atraccion_id_estado_cestado_atraccion_id_estado FOREIGN KEY (id_estado) REFERENCES cestado_atraccion(id_estado);
+  ADD CONSTRAINT fk_atraccion_id_estado_cestado_atraccion_id_estado_atraccion FOREIGN KEY (id_estado_atraccion) REFERENCES cestado_atraccion(id_estado_atraccion);
 
 ALTER TABLE atraccion
-  ADD CONSTRAINT fk_atraccion_id_tipo_atraccion_ctipo_atraccion_id_estado_atraccion FOREIGN KEY (id_tipo_atraccion) REFERENCES ctipo_atraccion(id_tipo_atraccion);
+  ADD CONSTRAINT fk_atraccion_id_tipo_atraccion_ctipo_atraccion_id_tipo_atraccion FOREIGN KEY (id_tipo_atraccion) REFERENCES ctipo_atraccion(id_tipo_atraccion);
 
 ALTER TABLE cliente 
   ADD CONSTRAINT fk_cliente_id_persona_persona_id_persona FOREIGN KEY (id_persona) REFERENCES persona(id_persona);
@@ -98,22 +105,28 @@ ALTER TABLE boleto
   ADD CONSTRAINT fk_boleto_id_cliente_cliente_id_cliente FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente);
 
 ALTER TABLE boleto 
-  ADD CONSTRAINT fk_boleto_id_tpo_boleto_ctipo_boleto_id_tipo_boleto FOREIGN KEY (id_tipo_boleto) REFERENCES ctipo_boleto(id_tipo_boleto);
+  ADD CONSTRAINT fk_boleto_id_tipo_boleto_ctipo_boleto_id_tipo_boleto FOREIGN KEY (id_tipo_boleto) REFERENCES ctipo_boleto(id_tipo_boleto);
 
 ALTER TABLE ccolonia 
   ADD CONSTRAINT fk_ccolonia_id_municipio_cmunicipio_id_municipio FOREIGN KEY (id_municipio) REFERENCES cmunicipio(id_municipio);
 
 ALTER TABLE articulo 
-  ADD CONSTRAINT fk_articulo_id_tipo_articulo_ctipo_articulo_id_tipo_articulo FOREIGN KEY (id_tipo_articulo) REFERENCES ctipo_articulo (id_tipo_articulo);
+  ADD CONSTRAINT fk_articulo_id_tipo_articulo_ctipo_articulo_id_tipo_articulo FOREIGN KEY (id_tipo_articulo) REFERENCES ctipo_articulo(id_tipo_articulo);
 
-ALTER TABLE boleto 
-  ADD CONSTRAINT fk_boleto_id_boleto_boleto_id_boleto FOREIGN KEY (id_boleto) REFERENCES boleto(id_boleto);
+ALTER TABLE articulo 
+  ADD CONSTRAINT fk_articulo_id_boleto_boleto_id_boleto FOREIGN KEY (id_boleto) REFERENCES boleto(id_boleto);
 
 ALTER TABLE orden
   ADD CONSTRAINT fk_orden_id_articulo_articulo_id_articulo FOREIGN KEY (id_articulo) REFERENCES articulo(id_articulo);
 
 ALTER TABLE persona
-  ADD CONSTRAINT fk_id_persona_id_sexo_sexo_id_sexo FOREIGN KEY (id_);
+  ADD CONSTRAINT fk_id_persona_id_sexo_csexo_id_sexo FOREIGN KEY (id_sexo) REFERENCES csexo(id_sexo);
+
+ALTER TABLE persona
+  ADD CONSTRAINT fk_id_persona_id_direccion_direccion_id_direccion FOREIGN KEY (id_direccion) REFERENCES direccion(id_direccion);
+
+ALTER TABLE cmunicipio
+  ADD CONSTRAINT fk_cmunicipio_id_estado_cestado_id_estado FOREIGN KEY (id_estado) REFERENCES cestado(id_estado);
 
 -------------------------NOT NULL----------------------------
 ALTER TABLE articulo
@@ -134,9 +147,6 @@ ALTER TABLE boleto
 ALTER TABLE boleto 
   ALTER COLUMN id_tipo_boleto SET NOT NULL;
 
-ALTER TABLE boleto 
-  ALTER COLUMN fecha_validez SET NOT NULL;
-
 ALTER TABLE ccolonia 
   ALTER COLUMN etiqueta_colonia SET NOT NULL;
 
@@ -148,6 +158,9 @@ ALTER TABLE cestado_atraccion
 
 ALTER TABLE cfabricante
   ALTER COLUMN etiqueta_fabricante SET NOT NULL;
+
+ALTER TABLE cmunicipio
+  ALTER COLUMN id_municipio
 
 ALTER TABLE ctipo_articulo 
   ALTER COLUMN etiqueta_tipo_articulo SET NOT NULL;
@@ -170,17 +183,11 @@ ALTER TABLE direccion
 ALTER TABLE empleado_atraccion
   ALTER COLUMN numero_veces_uso SET NOT NULL;
 
-ALTER TABLE empleado_atraccion
-  ALTER COLUMN fecha_uso SET NOT NULL;
-
 ALTER TABLE empleado
   ALTER COLUMN sueldo SET NOT NULL;
 
 ALTER TABLE empleado
   ALTER COLUMN nss SET NOT NULL;
-
-ALTER TABLE orden_cliente
-  ALTER COLUMN fecha_orden SET NOT NULL;
 
 ALTER TABLE orden_cliente
   ALTER COLUMN hora_pago SET NOT NULL;
@@ -219,13 +226,13 @@ ALTER TABLE persona
   ALTER COLUMN nombre SET NOT NULL;
 
 ALTER TABLE persona
-  ALTER COLUMN fecha_nacimiento SET NOT NULL;
-
-ALTER TABLE persona
   ALTER COLUMN id_sexo SET NOT NULL;
 
 ALTER TABLE persona
-  ALTER COLUMN id_direccion SET NOT NULL;
+  ALTER COLUMN id_direccion SET NOT NULL; 
+
+ALTER TABLE persona 
+  ALTER COLUMN telefono
 
 
 ---------------------------CHECKS----------------------------
@@ -242,6 +249,7 @@ ALTER TABLE direccion
 ALTER TABLE orden 
   ADD CHECK (cantidad > 0);
 
-
+---ya no sé que mas acá compas D:
+-- pues ya lo dejamos asi jeje no esta mal :)
 
 
